109205eddadd7b6769ef7c783ebc895fbe23c979
Sebastian <sebastiangm01@gmail.com>
2024-10-01 22:08:26 -0500

    Logo changed

--- streamlit_app.py
+++ streamlit_app.py
@@ -105,7 +105,7 @@ def generate_pdf(df):
     table.setStyle(style)
 
     # Add logo image
-    logo = Image("reports/figures/Logo Delegacion.png", width=60, height=60)
+    logo = Image("reports/figures/Logo Partido.png", width=60, height=60)
 
     # Title on the left
     title = Paragraph("Acciones de Libertad Religiosa, Consolidado Nacional", title_style)
