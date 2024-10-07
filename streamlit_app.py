41f72714bc1a4bcb711f5b203e92a2e520f05210
Sebastian <sebastiangm01@gmail.com>
2024-10-07 13:58:25 -0500

    Remove footer'

--- streamlit_app.py
+++ streamlit_app.py
@@ -228,6 +228,13 @@ def main():
             mime="application/pdf"
         )
 
+    hide_streamlit_style = """
+                <style>
+                #MainMenu {visibility: hidden;}
+                footer {visibility: hidden;}
+                </style>
+                """
+    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     
 if __name__ == "__main__":
     main()
