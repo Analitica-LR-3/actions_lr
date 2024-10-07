a5562aa5d4136f9ae1fd32d195aa86e3d88f06b5
Sebastian <sebastiangm01@gmail.com>
2024-10-07 15:26:41 -0500

    Remove menu and footer hidden

--- streamlit_app.py
+++ streamlit_app.py
@@ -227,15 +227,6 @@ def main():
             file_name=f"acciones_diarias-{now}.pdf",
             mime="application/pdf"
         )
-
-    # Hide Streamlit's menu and footer
-    hide_streamlit_style = """
-                <style>
-                #MainMenu {visibility: hidden;}
-                footer {visibility: hidden;}
-                </style>
-                """
-    st.markdown(hide_streamlit_style, unsafe_allow_html=True)
     
 if __name__ == "__main__":
     main()
